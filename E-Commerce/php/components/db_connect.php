d0b6522537e48cbb9270967a6132417671be6766
Martin Obermayer <59625525+mobermayer@users.noreply.github.com>
2021-05-19 11:29:32 +0200

    fix: db connection

--- E-Commerce/php/components/db_connect.php
+++ E-Commerce/php/components/db_connect.php
@@ -1,16 +1,13 @@
 <?php
 $serverName = "173.212.235.205";
-$userName = "obermaye_team5";
+$userName = "obermaye_userT5";
 $password = "394hgid45nHJ8";
 $databaseName = "obermaye_wf-backend-5-ecommerce";
-
-echo "hallo";
 
 $conn = mysqli_connect($serverName, $userName, $password, $databaseName);
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 } else {
-    echo "DB connected";
+    // echo "DB connected";
 }
-?>

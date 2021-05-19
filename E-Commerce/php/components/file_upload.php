1f8cca8218da9ffbd93c859e7ab208a80744c2c6
MarkoGrah0vac7 <marko.grahovac7@gmail.com>
2021-05-19 16:07:42 +0200

    register changes

--- E-Commerce/php/components/file_upload.php
+++ E-Commerce/php/components/file_upload.php
@@ -11,7 +11,7 @@ function file_upload($image, $source = 'user')
     $fileError = $image["error"];
     $fileSize = $image["size"];
     $fileExtension = strtolower(pathinfo($fileName,PATHINFO_EXTENSION));    
-    $filesAllowed = ["png", "jpg", "jpeg"];
+    $filesAllowed = ["png", "jpg", "jpeg", "webp"];
     if ($fileError == 4) {       
         $result->ErrorMessage = "No image was chosen. It can always be updated later.";
         return $result;
@@ -21,10 +21,10 @@ function file_upload($image, $source = 'user')
                 if ($fileSize < 1500000) { //1500kb this number is in bytes
                     //it gives a file name based microseconds
                     $fileNewName = uniqid('') . "." . $fileExtension; // 1233343434.jpg i.e
-                    if($source == 'offer'){
-                        $destination = "../../img/$fileNewName";
+                    if($source == 'product'){
+                        $destination = "../../img/product_images/$fileNewName";
                     }elseif ($source == 'user'){
-                        $destination = "img/$fileNewName";
+                        $destination = "../img/user_images/$fileNewName";
                     }                    
                     if (move_uploaded_file($fileTmpName, $destination)) {
                         $result->error = 0;

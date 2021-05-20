2e7ec6e7acfeed182d8fbefc0d3f0e9737217405
Lesia Hrynko <melaverde1987@gmail.com>
2021-05-20 15:48:16 +0200

    user page styling

--- E-Commerce/php/user/profile.php
+++ E-Commerce/php/user/profile.php
@@ -42,77 +42,89 @@ if ($_GET['id']) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Profile</title>
     <?php require_once '../components/boot.php'?>
-    <link rel='stylesheet' type='text/css' href='styles.css'>
+    <link rel="stylesheet" href="../../style/main-style.css" />
 </head>
 
 <body>
-    <div id="container">
-        <div id="content">
-            <h1>Profile</h1>
-            <?php
-            if (isset($errMSG)) {
+    <?php 
+        require_once '../components/header.php';
+        navbar("../../");
+    ?>
+    <div id="container" class="container">
+        <div class="row my-5 py-5">
+            <div class="col-12 fs_6 text-uppercase my-2">Profile</div>
+            <div id="content">
+                <?php
+                if (isset($errMSG)) {
+                    ?>
+                <div class="alert alert-<?php echo $errTyp ?>">
+                    <p><?php echo $errMSG; ?></p>
+                </div>
+                <?php
+                }
                 ?>
-            <div class="alert alert-<?php echo $errTyp ?>">
-                <p><?php echo $errMSG; ?></p>
-            </div>
-            <?php
-            }
-            ?>
-            <div>
-                <?php 
-                    if ($role == 'admin') {
-                        echo "<img src='../../img/default-admin.jpg'>";
-                    } else {
-                        echo "<img src='../../img/default-user.jpg'>";
-                    }
-                
-                ?>
-            </div>
-            <table class='table'>
-                <tr>
-                    <th>First name</th>
-                    <td>
-                        <?php echo $firstName ?>
-                    </td>
-                </tr>
-                <tr>
-                    <th>Last name</th>
-                    <td>
-                        <?php echo $lastName ?>
-                    </td>
-                </tr>
-                <tr>
-                    <th>Address</th>
-                    <td>
-                        <?php echo $address ?>
-                    </td>
-                </tr>
-                <tr>
-                    <th>City</th>
-                    <td><?php echo $city ?></td>
-                </tr>
-                <tr>
-                    <th>Postcode</th>
-                    <td><?php echo $postcode ?></td>
-                </tr>
-                <tr>
-                    <th>Country</th>
-                    <td><?php echo $country ?></td>
-                </tr>
-                <tr>
-                    <th>Birthdate</th>
-                    <td><?php echo $birthdate ?></td>
-                </tr>
-                <tr>
-                    <th><a href="javascript:history.back()"><button class='btn btn-light' type="button">Back</button></a></th>
-                    <td>
-                        <a href='profile-update.php?id="<?php echo $id ?>"'><button class='btn btn-secondary' type='button'>Update</button></a>
-                    </td>
-                </tr>
-            </table>
 
-            
+                <div class="col-12 col-md-2">
+                    <?php 
+                        if ($role == 'admin') {
+                            echo "<img src='../../img/user_images/default-admin.jpg'>";
+                        } else {
+                            echo "<img src='../../img/user_images/default-user.jpg'>";
+                        }
+                    
+                    ?>
+                </div>
+                <table class='table'>
+                    <tr>
+                        <th>First name</th>
+                        <td>
+                            <?php echo $firstName ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>Last name</th>
+                        <td>
+                            <?php echo $lastName ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>Address</th>
+                        <td>
+                            <?php echo $address ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>City</th>
+                        <td><?php echo $city ?></td>
+                    </tr>
+                    <tr>
+                        <th>Postcode</th>
+                        <td><?php echo $postcode ?></td>
+                    </tr>
+                    <tr>
+                        <th>Country</th>
+                        <td><?php echo $country ?></td>
+                    </tr>
+                    <tr>
+                        <th>Birthdate</th>
+                        <td><?php echo $birthdate ?></td>
+                    </tr>
+                    <tr>
+                        <th><a href="javascript:history.back()"><button class='btn btn bg_lightgray bg_hover rounded-pill col-12 col-md-auto py-2 px-4 text-white my-md-3' type="button">Back</button></a></th>
+                        <td>
+                            <a href='profile-update.php?id="<?php echo $id ?>"'><button class='btn btn bg_gray bg_hover rounded-pill col-12 col-md-auto py-2 px-4 text-white my-md-3 mb-2' type='button'>Update</button></a>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </div>
     </div>
+
+    <?php 
+        require_once '../components/footer.php';
+        footer("../../");
+        require_once '../components/boot-javascript.php';
+    ?>
 </body>
 
 </html>

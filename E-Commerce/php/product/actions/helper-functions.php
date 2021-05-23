0d5a607332543896c39b3cfc37facd3315162bbc
Maria Schmelzer-Ziringer <mariasz@gmail.com>
2021-05-23 02:58:10 +0200

    changes

---
+++ E-Commerce/php/product/actions/helper-functions.php
@@ -0,0 +1,30 @@
+<?php 
+
+function discountedPrice($price, $discount_procent) {
+    return $price / 100 * (100-$discount_procent);
+}
+function getStars($rating) {
+    $stars = "";
+    switch($rating){
+        case 1:
+            $stars = "★☆☆☆☆";
+            break;
+        case 2:
+            $stars = "★★☆☆☆";
+            break;
+        case 3:
+            $stars = "★★★☆☆";
+            break;
+        case 4:
+            $stars = "★★★★☆";
+            break;
+        case 5:
+            $stars = "★★★★★";
+            break;
+        default:
+            $stars = "☆☆☆☆☆";
+    }
+    return $stars;    
+}
+
+?>

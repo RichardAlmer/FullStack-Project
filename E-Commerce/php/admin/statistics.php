456368a3db2eba246289fae1aaf4f4cfa28f5718
Maria Schmelzer-Ziringer <mariasz@gmail.com>
2021-05-23 03:15:54 +0200

    basic working version

--- E-Commerce/php/admin/statistics.php
+++ E-Commerce/php/admin/statistics.php
@@ -1,7 +1,48 @@
 <?php
-// To Do: Session stuff ---------------------------------------
+    require_once '../components/db_connect.php';
 
-// To Do: DB stuff  ---------------------------------------
+    session_start();
+    if (!isset($_SESSION['admin']) && !isset($_SESSION['user'])) {
+        header("Location: ../../index.php");
+        exit;
+    }
+    if (isset($_SESSION["user"])) {
+        header("Location: ../product/product-catalog.php");
+        exit;
+    }
+
+    $sql = "SELECT product.name AS name, product.category AS category, product.brand AS category,brand, purchase_item.quantity AS quantity
+            FROM product 
+            INNER JOIN purchase_item ON purchase_item.pk_purchase_item_id = product.pk_product_id
+            ORDER BY purchase_item.quantity DESC";
+    $result = mysqli_query($conn ,$sql);     
+
+    $htmlResult="<table class='table table-striped'>
+                    <thead class='table-success'>
+                        <tr>
+                            <th>Product Name</th>
+                            <th>Category</th>
+                            <th>Brand</th>
+                            <th>Quantity</th>
+                        </tr>
+                    </thead>
+                    <tbody>"; 
+    if(mysqli_num_rows($result) > 0) {     
+        while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){ 
+            $htmlResult .= "
+            <tr>
+                <td>" . $row['name'] . "</td>
+                <td>" . $row['category'] . "</td>
+                <td>" . $row['brand'] . "</td>
+                <th>" . $row['quantity'] . "</th>
+            </tr>";
+        };
+    } else  {
+    $htmlResult =  "<center>No Data Available </center>";
+    }
+    $htmlResult .= "</tbody></table>";
+
+    $conn->close();
 
 ?>
 
@@ -11,18 +52,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sales Statistics</title>
-    <?php require_once '../components/boot.php'?>
-    <!-- <link rel='stylesheet' type='text/css' href='../../main.css'> -->
+    <title>Statistics</title>
+    <?php require_once '../components/boot.php' ?>
+    <link rel="stylesheet" href="../../style/main-style.css" />
 </head>
 
 <body>
-    <div id="container">
-        <?php include '../components/navbar.php' ?>
-        <div id="content">
-            <h1>Sales Statistics</h1>
+
+    <?php
+        require_once '../components/header.php';
+        navbar("../../", "../");
+    ?>
+
+    <div id="container" class="container">
+        <div id="content" class="my-5 py-5">
+            <div class="col-12 fs_6 text-uppercase my-2">Statistics</div>
             <table>
-                <!-- To Do -->
+                <?= $htmlResult ?>
             </table>
             <a href='dashboard.php'>Back to dashboard</a>
         </div>

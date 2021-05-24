6f38c338f30f37c43a7df8f151be4ba126c290ac
Richard Almer <rarichardalmer@gmail.com>
2021-05-24 15:59:24 +0200

    footer

--- E-Commerce/php/components/footer.php
+++ E-Commerce/php/components/footer.php
@@ -4,19 +4,22 @@ echo '
 <footer class="bg_gray py-5">
     <div class="container py-5">
         <div class="row justify-content-between">
-            <div class="col-12 col-lg-6 mb-5 mb-lg-0 text-center text-lg-start">
+            <div class="col-12 col-lg-4 mb-5 mb-lg-0 text-center text-lg-start">
                 <img class="logo col-auto" src="'.$level.'img/general_images/logo-white.png" alt="">
             </div>
 
-            <div class="col-12 col-lg-6 text-center text-lg-end">
+            <div class="col-12 col-lg-8 text-center text-lg-end">
                 <div class="row">
-                    <div class="col-12 col-md-4 mb-2 mb-md-0">
-                        <a class="px-3 text-white text-center text-md-end" href="/">Impressum</a>
+                    <div class="col-12 col-md-3 mb-2 mb-md-0">
+                        <a class="px-3 text-white text-center text-md-end" href="'.$level.'html/imprint.html">Impressum</a>
                     </div>
-                    <div class="col-12 col-md-4 mb-2 mb-md-0">
+                    <div class="col-12 col-md-3 mb-2 mb-md-0">
+                        <a class="px-3 text-white text-center text-md-end" href="'.$level.'html/dataProtection.html">Data-Protection</a>
+                    </div>
+                    <div class="col-12 col-md-3 mb-2 mb-md-0">
                         <a class="px-3 text-white text-center text-md-end" href="/">About us</a>
                     </div>
-                    <div class="col-12 col-md-4 mb-2 mb-md-0">
+                    <div class="col-12 col-md-3 mb-2 mb-md-0">
                         <a class="px-3 text-white text-center text-md-end" href="'.$level.'php/contact.php">Contact</a>
                     </div>
                 </div>

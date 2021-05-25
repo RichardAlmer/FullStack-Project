046ac8adf0bcd9d985e62c5b937b4053da66d265
Richard Almer <rarichardalmer@gmail.com>
2021-05-25 15:48:08 +0200

    about us link to #

--- E-Commerce/php/components/footer.php
+++ E-Commerce/php/components/footer.php
@@ -17,7 +17,7 @@ echo '
                         <a class="px-3 text-white text-center text-md-end" href="'.$level.'html/dataProtection.html">Data-Protection</a>
                     </div>
                     <div class="col-12 col-md-3 mb-2 mb-md-0">
-                        <a class="px-3 text-white text-center text-md-end" href="/">About us</a>
+                        <a class="px-3 text-white text-center text-md-end" href="#">About us</a>
                     </div>
                     <div class="col-12 col-md-3 mb-2 mb-md-0">
                         <a class="px-3 text-white text-center text-md-end" href="'.$level.'php/contact.php">Contact</a>

910b93337f550fb9cdf364774107d4778a2fccc7
Maria Schmelzer-Ziringer <mariasz@gmail.com>
2021-05-25 11:50:10 +0200

    fixed sql query

--- E-Commerce/php/admin/statistics.php
+++ E-Commerce/php/admin/statistics.php
@@ -11,10 +11,11 @@
         exit;
     }
 
-    $sql = "SELECT product.name AS name, product.category AS category, product.brand AS category,brand, purchase_item.quantity AS quantity
-            FROM product 
-            INNER JOIN purchase_item ON purchase_item.pk_purchase_item_id = product.pk_product_id
-            ORDER BY purchase_item.quantity DESC";
+    $sql = "SELECT product.name, product.category, product.brand, purchase_item.fk_product_id, SUM(purchase_item.quantity) AS quantity 
+    FROM purchase_item 
+    INNER JOIN product ON purchase_item.fk_product_id = product.pk_product_id
+    GROUP BY purchase_item.fk_product_id
+    ORDER BY quantity DESC";
     $result = mysqli_query($conn ,$sql);     
 
     $htmlResult="<table class='table table-striped'>
@@ -27,7 +28,7 @@
                         </tr>
                     </thead>
                     <tbody>"; 
-    if(mysqli_num_rows($result) > 0) {     
+    if ($result->num_rows > 0) {  
         while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){ 
             $htmlResult .= "
             <tr>
